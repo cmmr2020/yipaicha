7bf926090ba12e657d2a257b88a1b9d53c435bb7
unknown <unknown@example.com>
2021-04-20 09:29:28 +0800

    Initial Commit

---
+++ pages/editInfo/editInfo.js
@@ -0,0 +1,145 @@
+// pages/editInfo/editInfo.js
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    openId:'',
+    uname:'',
+    gname:'',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var openId = options.openid;
+    if(!openId){
+      wx.showToast({
+        title: '获取用户失败!',
+        icon: 'none',
+        duration: 2000,
+        mask: true
+      })
+      return;
+    }else{
+      this.setData({
+        openId: options.openid,
+    })
+    wx.request({
+      url: app.globalData.requestUrl+'/member/manage/getUser',
+      method: "GET",
+      data:{
+        openId:options.openid,
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success:(res)=>{
+        if (res.data.status == 'success') {
+          //console.log(res)
+          var obj = res.data.retObj;
+          if(!obj.uname){
+            return
+          }
+          var gname = '';
+          if(obj.groupNname){
+            gname = obj.groupNname;
+          }
+          this.setData({
+            uname:obj.uname,
+            gname:gname
+          })
+        }else{
+          wx.showToast({
+            title: '系统错误!',
+            icon: 'none',
+            duration: 1500,
+            mask: true
+          })
+        }
+      },
+      fail:()=>{
+        wx.showToast({
+          title: '系统错误!',
+          icon: 'none',
+          duration: 1500,
+          mask: true
+        })
+      }
+    })
+    }
+  },
+
+  textareaAInput(e) {
+    this.setData({
+      uname: e.detail.value
+    })
+  },
+  textareaBInput(e) {
+    this.setData({
+      gname: e.detail.value
+    })
+  },
+  back(){
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  save(){
+    var that = this;
+    var uname = that.data.uname;
+    var gname = that.data.gname;
+    if(!uname){
+      wx.showToast({
+        title: '请输入您的姓名~',
+        icon: 'none',
+        duration: 1500,
+        mask: true
+      })
+    }else{
+      wx.request({
+        url: app.globalData.requestUrl+'/member/manage/updateUserForInfo',
+        method: "POST",
+        data:{
+          openId:that.data.openId,
+          uname:uname,
+          gname:gname,
+        },
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        success:(res)=>{
+          if (res.data.status == 'success') {
+            wx.showToast({
+              title: '成功',
+              icon: 'none',
+              duration: 1500,
+              mask: true,
+              success:()=>{
+                that.back();
+              }
+            })
+          }else{
+            wx.showToast({
+              title: '系统错误!',
+              icon: 'none',
+              duration: 1500,
+              mask: true
+            })
+          }
+        },
+        fail:()=>{
+          wx.showToast({
+            title: '系统错误!',
+            icon: 'none',
+            duration: 1500,
+            mask: true
+          })
+        }
+      })
+    }
+  }
+})
